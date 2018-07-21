0e27ea77000ebf6019da40c3a35cc01712b17c2d
AMutaa <piusandru@gmail.com>
2018-07-21 19:15:01 -0400

    updated css '

--- app/components/Compare.js
+++ app/components/Compare.js
@@ -1,10 +1,152 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class Compare extends Component {
+function PlayerPreview(props) {
+  return (
+    <div>
+      <div className="column">
+        <img
+          className="avatar"
+          src={props.avatar}
+          alt={'Avatar for'}
+        />
+
+        <h2 className="username">@{props.username}</h2>
+        <button
+          className="reset"
+          onClick={props.onReset.bind(null, props.id)}
+        >Reset</button>
+      </div>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+}
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    var value = event.target.value;
+    this.setState(() => ({
+      username: value,
+    }));
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.props.id, this.props.username);
+  }
   render() {
     return (
+      <form className="column" onSubmit={this.handleSubmit}>
+        <label className="header" htmlFor="username">
+          {this.props.label}
+        </label>
+        <input
+          type="text"
+          id="username"
+          placeholder="github username"
+          autoComplete="off"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+
+        <button className="button" type="submit" disabled={!this.state.username}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+PlayerInput.defaultProps = {
+  label: 'Username',
+};
+class Compare extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  handleSubmit(id, username) {
+    this.setState(() => {
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com' + username + '.png?size=200';
+      return newState;
+    });
+  }
+
+  handleReset(id) {
+    this.setState(function () {
+      var newState = {};
+      newState[id + 'Name'] = '';
+      newState[id + 'Image'] = null;
+      return newState;
+
+    })
+  }
+
+  render() {
+    var playerOneName = this.state.playerOneName;
+    var playerTwoName = this.state.playerTwoName;
+    var playerOneImage = this.state.playerOneImage;
+    var playerTwoImage = this.state.playerTwoImage;
+
+    return (
       <div>
-        <h1>COMPARE</h1>
+        <div className="row">
+          {!playerOneName &&
+            <PlayerInput id="playerOne" label="Player One" onSubmit={this.handleSubmit} />}
+
+          {playerOneImage !== null &&
+            <PlayerPreview
+              avatar={playerOneImage}
+              username={playerOneName}
+              onReset={this.handleReset}
+              id='playerOne'
+            />}
+
+
+          {!playerTwoName &&
+            <PlayerInput id="playerTwo" label="Player Two" onSubmit={this.handleSubmit} />}
+
+          {playerTwoImage !== null &&
+            <PlayerPreview
+              avatar={playerTwoImage}
+              username={playerTwoName}
+              onReset={this.handleReset}
+              id='playerTwo'
+            />}
+
+        </div>
       </div>
     );
   }
