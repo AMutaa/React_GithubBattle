f3be93ae33d6a8e770458b479c64e6234b477acb
AMutaa <piusandru@gmail.com>
2018-08-21 19:19:13 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require('path');
 const HtmlWebapackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        ],
+    },
+
+    devServer: {
+        historyApiFallback: true,
+    },
+
+    plugins: [
+        new HtmlWebapackPlugin({
+            template: 'app/index.html',
+        }),
     ],
-  },
-
-  devServer: {
-    historyApiFallback: true,
-  },
-
-  plugins: [
-    new HtmlWebapackPlugin({
-      template: 'app/index.html',
-    }),
-  ],
-  mode: 'development',
+    mode: 'development',
 };
